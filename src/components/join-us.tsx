7a842ddbfce5f50c72ee1a356dac833ade297b00
mohamed rhanmi <106605846+morhaaa@users.noreply.github.com>
2023-10-30 18:00:45 +0100

    Implement JoinUs section

--- src/components/join-us.tsx
+++ src/components/join-us.tsx
@@ -1,19 +1,25 @@
 const JoinUs: React.FC = () => {
   return (
-    <section className="h-[50vh] w-full flex flex-col items-center justify-center gap-20">
-      <div className="flex flex-col items-center gap-3 text-white">
-        <h2 className="text-8xl font-bold">Join Us</h2>
+    <section className="w-full flex flex-col items-center justify-center pb-28 pt-10 gap-20 bg-black/80">
+      <div className="flex flex-col justify-center gap-4 items-center text-white text-8xl ">
+        <h1 className=" font-bold">
+         Join{" "}
+          <span className="text-transparent bg-clip-text bg-gradient-to-br from-fuchsia-300 to-fuchsia-500">
+           Us
+          </span>{" "}
+        </h1>
+        <span className="text-base text-center text-slate-400">Join the growing community of over 5518 satisfied users who have experienced the power of the AI Icon Generator App.<br/> Whether you're a designer, developer, marketer, or simply someone who loves to add visual flair to your projects, this app is a game-changer.</span>
       </div>
       <div className="flex gap-60">
         <div className="flex flex-col items-center">
-          <p className="font-extrabold text-transparent text-8xl bg-clip-text bg-gradient-to-br from-emerald-400 to-green-600">
+          <p className="font-extrabold text-transparent text-8xl bg-clip-text  bg-gradient-to-br from-fuchsia-300 to-fuchsia-500">
             2300
           </p>
           <p className="text-white text-4xl">Users</p>
         </div>
 
         <div className="flex flex-col items-center">
-          <p className="font-extrabold text-transparent text-8xl bg-clip-text bg-gradient-to-br from-emerald-400 to-green-600">
+          <p className="font-extrabold text-transparent text-8xl bg-clip-text bg-gradient-to-br from-fuchsia-300 to-fuchsia-500">
             2500
           </p>
           <p className="text-white text-4xl">Icons Generated</p>
--- src/components/tutorial.tsx
+++ src/components/tutorial.tsx
@@ -12,8 +12,8 @@ import {
 
 const Tutorial: React.FC = () => {
   return (
-    <section className="w-full h-full flex flex-col gap-20 py-20 bg-gradient-to-b from-transparent via-black/30 to-black/80">
-      <div className="flex flex-col justify-center items-center text-white pt-20 text-6xl ">
+    <section className="w-full h-full flex flex-col gap-6 py-20 px-10 bg-gradient-to-b from-transparent via-black/30 to-black/80 ">
+      <div className="flex flex-col justify-center items-center text-white pt-20 text-7xl ">
         <h1 className=" font-bold">
           How does it{" "}
           <span className="text-transparent bg-clip-text bg-gradient-to-br from-fuchsia-300 to-fuchsia-500">
@@ -25,15 +25,15 @@ const Tutorial: React.FC = () => {
       <div className="text-white flex items-center gap-10 px-10 py-10">
         <div className="basis-1/2 border-l-4  border-slate-50 pl-10">
           <h4 className=" text-3xl font-bold">Step 1: Enter a prompt</h4>
-          <p className=" text-lg">
+          <p className=" text-slate-400 py-2 text-lg">
             The first step in generating an icon is entering a prompt. A prompt
             is a short description or context that helps us understand the icon
             you want to create.
           </p>
         </div>
-        <div className="basis-1/2">
+        <div className="basis-1/2 flex justify-center">
           <NeonBorder>
-            <div className="w-[50rem]">
+            <div className="w-[45rem]">
               <InputGenerator
                 size="medium"
                 placeholder="Enter your prompt"
@@ -63,7 +63,7 @@ const Tutorial: React.FC = () => {
         </div>
         <div className="basis-1/2  border-r-4  border-slate-50 pr-10 text-right">
           <h4 className=" text-3xl font-bold">Step 2: Select a color</h4>
-          <p className=" text-lg">
+          <p className="  text-slate-400 py-2 text-lg">
             In this step, choose the color for your icon. Pick the color that
             best matches your vision for the icon
           </p>
@@ -74,7 +74,7 @@ const Tutorial: React.FC = () => {
        <div className="text-white flex items-center gap-10 px-10 py-10">
        <div className="basis-1/2  border-l-4  border-slate-50 pl-10 text-left">
           <h4 className=" text-3xl font-bold">Step 3: Select a style</h4>
-          <p className=" text-lg">
+          <p className="  text-slate-400 py-2  text-lg">
             Here you can choose the style for your icon. We offer a variety of options for your needs
           </p>
         </div>
@@ -130,7 +130,7 @@ const Tutorial: React.FC = () => {
         </div>
         <div className="basis-1/2  border-r-4  border-slate-50 pr-10 text-right">
           <h4 className=" text-3xl font-bold">Step 4: Choose number for your icons</h4>
-          <p className=" text-lg">
+          <p className="  text-slate-400 py-2  text-lg">
            Finally, you specify the number of icons you want to generate. Whether you need a single icon or a set of multiple icons
           </p>
         </div>
