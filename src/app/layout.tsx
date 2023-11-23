94f2a5ce772848f6b044c31b2b8052ac3bee71e8
Mohamed <mohamed.rhanmi@gmail.com>
2023-11-23 17:24:12 +0100

    Add progress bar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ import { Toaster } from "@/components/ui/toaster";
 import Modal from "@/components/modal-credits";
 import { Inter } from "next/font/google";
 import { cn } from "@/lib/utils";
+import NextTopLoader from "nextjs-toploader";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -33,7 +34,20 @@ export default function RootLayout({
           <Toaster />
           <Navbar />
           <Modal />
-          <main className="h-full w-full overflow-scroll">{children}</main>
+          <main className="h-full w-full overflow-scroll">
+            <NextTopLoader
+              color="#2299DD"
+              initialPosition={0.08}
+              crawlSpeed={200}
+              height={3}
+              crawl={true}
+              showSpinner={true}
+              easing="ease"
+              speed={200}
+              shadow="0 0 10px #2299DD,0 0 5px #2299DD"
+            />
+            {children}
+          </main>
         </body>
       </html>
     </Providers>
--- src/containers/providers.tsx
+++ src/containers/providers.tsx
@@ -3,7 +3,6 @@ import React, { ReactNode } from "react";
 import { Provider } from "react-redux";
 import { store } from "./store";
 import { SessionProvider } from "next-auth/react";
-import Modal from "@/components/modal-credits";
 
 interface ProvidersProps {
   children: ReactNode;
