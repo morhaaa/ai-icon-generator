a93403b0da42b8481c468a3d15d06d08f2194071
mohamed rhanmi <106605846+morhaaa@users.noreply.github.com>
2023-12-28 10:30:22 +0100

    Create Terms and Conditions Page

---
+++ src/app/terms-and-conditions/page.tsx
@@ -0,0 +1,136 @@
+import Footer from '@/components/footer'
+import React from 'react'
+
+export default function TermsAndConditions() {
+  return (
+    <section className="h-full w-full overflow-auto">
+      <div className="px-60 my-20 text-slate-300">
+        <h1 className="text-2xl font-bold mb-4">Terms of Service</h1>
+        <p className="mb-4">Effective Date: June 12, 2023</p>
+
+        <p className="mb-4">
+          Please carefully review these terms and conditions before utilizing Our Service.
+        </p>
+
+        <p className="mb-4">
+          These Terms of Service ("Terms") regulate your access to and use of the yummygenerator.io
+          website ("Website"), encompassing any content, features, or services provided by Yummy
+          Generator ("Yummy Generator," "we," "us," or "our"). By accessing or utilizing the
+          Website, you consent to be bound by these Terms. If you disagree with any part of these
+          Terms, you may not access the Website.
+        </p>
+
+        <h2 className="text-xl font-bold mb-2">Acceptance of Terms</h2>
+        <p className="mb-4">
+          By accessing or utilizing the Website, you confirm that you are of legal age to consent to
+          these Terms, or you have secured parental or guardian approval. If you are accessing or
+          using the Website on behalf of a company or another legal entity, you assert and guarantee
+          that you have the authority to bind such entity to these Terms.
+        </p>
+
+        <h2 className="text-xl font-bold mb-2">Description of Service</h2>
+        <p className="mb-4">
+          Yummy Generator offers a platform enabling users to create logos based on prompts. Users
+          can enter and use the Website to design and customize logos for personal or commercial use,
+          subject to these Terms.
+        </p>
+
+        <h2 className="text-xl font-bold mb-2">User Accounts</h2>
+        <h3 className="text-lg font-bold mb-2">3.1 Account Creation</h3>
+        <p className="mb-4">
+          To unlock specific features of the Website, you may need to establish a user account. You
+          agree to provide precise, current, and comprehensive information during the account
+          registration process. You are solely responsible for maintaining the secrecy of your account
+          credentials and for any activity transpiring under your account. You agree to promptly
+          inform Yummy Generator of any unauthorized use of your account.
+        </p>
+
+        <h3 className="text-lg font-bold mb-2">3.2 Account Termination</h3>
+        <p className="mb-4">
+          Yummy Generator retains the right to suspend or terminate your account, at its sole
+          discretion, at any time and for any reason, including, but not limited to, a breach of
+          these Terms or any applicable law. Upon termination, your right to access and use the
+          Website will cease immediately.
+        </p>
+
+        <h2 className="text-xl font-bold mb-2">User Content</h2>
+        <h3 className="text-lg font-bold mb-2">4.1 License Grant</h3>
+        <p className="mb-4">
+          By submitting, posting, or uploading any content to the Website, you grant Yummy Generator a
+          non-exclusive, global, royalty-free, sublicensable, and transferable license to use,
+          reproduce, distribute, prepare derivative works of, display, and perform such content in
+          connection with the Website and Yummy Generator's business.
+        </p>
+
+        <h3 className="text-lg font-bold mb-2">4.2 Responsibility for User Content</h3>
+        <p className="mb-4">
+          You are exclusively responsible for any content you submit, post, or upload to the Website.
+          You assert and guarantee that you possess all necessary rights and permissions to grant the
+          license outlined in Section 4.1, and that your content does not infringe upon the rights of
+          any third party.
+        </p>
+
+        <h3 className="text-lg font-bold mb-2">4.3 Prohibited Content</h3>
+        <p className="mb-4">
+          You agree not to submit, post, or upload any content that is unlawful, detrimental,
+          menacing, abusive, defamatory, indecent, or otherwise objectionable. Yummy Generator reserves
+          the right to delete or deactivate any content violating these Terms or deemed
+          inappropriate at our sole discretion.
+        </p>
+
+        <h2 className="text-xl font-bold mb-2">Intellectual Property Rights</h2>
+        <p className="mb-4">
+          The Website and its content, including, but not limited to, logos, text, graphics, images,
+          icons, software, and audio clips, are the property of Yummy Generator or its licensors and
+          are safeguarded by intellectual property laws. You may not replicate, distribute, alter,
+          create derivative works of, publicly display, or publicly perform any part of the Website or
+          its content without prior written consent from Yummy Generator.
+        </p>
+
+        <h2 className="text-xl font-bold mb-2">Limitation of Liability</h2>
+        <p className="mb-4">
+          To the utmost extent permitted by applicable law, Yummy Generator and its affiliates,
+          officers, directors, employees, agents, and suppliers shall not be liable for any indirect,
+          incidental, special, consequential, or punitive damages, including, but not limited to, loss
+          of profits, data, or goodwill, arising out of or in connection with these Terms or the use
+          or inability to use the Website.
+        </p>
+
+        <h2 className="text-xl font-bold mb-2">Indemnification</h2>
+        <p className="mb-4">
+          You agree to indemnify, defend, and hold harmless Yummy Generator and its affiliates,
+          officers, directors, employees, agents, and suppliers from and against any claims,
+          liabilities, damages, losses, costs, or expenses, including reasonable attorneys' fees,
+          arising out of or in connection with your use of the Website or any violation of these
+          Terms.
+        </p>
+
+        <h2 className="text-xl font-bold mb-2">Modifications to Terms</h2>
+        <p className="mb-4">
+          Yummy Generator retains the right to amend or substitute these Terms at any time at its sole
+          discretion. Any alterations will take effect immediately upon posting on the Website. Your
+          continued use of the Website after the posting of any changes constitutes your acceptance
+          of such modifications.
+        </p>
+
+        <h2 className="text-xl font-bold mb-2">Governing Law and Jurisdiction</h2>
+        <p className="mb-4">
+          The laws of the Country, excluding its conflicts of law rules, shall govern these Terms and
+          your use of the Service. Your use of the Application may also be subject to other local,
+          state, national, or international laws.
+        </p>
+
+        <h2 className="text-xl font-bold mb-2">Disputes Resolution</h2>
+        <p className="mb-4">
+          If you have any concerns or disputes about the Service, you agree to initially attempt to
+          resolve the dispute informally by contacting the Company.
+        </p>
+
+        <h2 className="text-xl font-bold mb-2">Contact Us</h2>
+        <p className="mb-4">If you have any questions about these Terms and Conditions, you can contact us:</p>
+        <p className="mb-4">By email: yummygenerator@gmail.com</p>
+      </div>
+      <Footer/>
+    </section>
+  )
+}
