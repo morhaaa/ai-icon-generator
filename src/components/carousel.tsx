11f413d054dd7933e15330f00f3666e7c358e6ae
MohaCodeploy <mohamed.rhanmi@gmail.com>
2024-04-08 14:55:00 +0200

    fix(carousel): fix carousel over-scroll

--- src/components/carousel.tsx
+++ src/components/carousel.tsx
@@ -8,7 +8,7 @@ interface CarouselsProps {
 }
 const Carousels: React.FC<CarouselsProps> = ({ direction, images }) => {
   return (
-    <div className="overflow-scroll relative w-full  h-28">
+    <div className="overflow-hidden relative w-full  h-28">
       <div
         className={clsx(
           "flex absolute left-0 justify-center items-center gap-8 w-[100%] ",
@@ -24,10 +24,8 @@ const Carousels: React.FC<CarouselsProps> = ({ direction, images }) => {
             width={100}
             height={100}
             className="rounded-md opacity-50"
-            unoptimized
             priority
-            quality={20}
-            blurDataURL={`/assets/icons-style/${img}`}
+            quality={0}
           />
         ))}
       </div>
